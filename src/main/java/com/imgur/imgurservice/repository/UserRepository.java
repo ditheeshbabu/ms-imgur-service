c305ad96ee40f3a5ccf4baaf22e4f05f62b17110
ditheesh <ditheesh@users.noreply.github.com>
2024-11-26 21:58:50 -0600

    feat: Tweaking the service and added a new endpoint for live service

--- src/main/java/com/imgur/imgurservice/repository/UserRepository.java
+++ src/main/java/com/imgur/imgurservice/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.imgur.imgurservice.repository;
 import com.imgur.imgurservice.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -10,6 +12,7 @@ import java.util.Optional;
  * Repository interface for performing database operations on the User entity.
  */
 @Repository
+@Transactional(isolation = Isolation.READ_COMMITTED)
 public interface UserRepository extends JpaRepository<UserEntity, String> {
 
     /**
